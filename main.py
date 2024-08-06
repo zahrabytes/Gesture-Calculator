f4a53b7e4914ad3533b2fdf2a3deaa44c920a430
Zahra Bukhari <bukhaz99@gmail.com>
2024-08-05 22:12:19 -0500

    drawing something

--- main.py
+++ main.py
@@ -5,9 +5,41 @@ from cvzone.HandTrackingModule import HandDetector
 # Initialize the webcam to capture video
 # The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
 cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4, 720)
 
 # Initialize the HandDetector class with the given parameters
-detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
+
+def getHandInfo(img):
+    # Find hands in the current frame
+    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
+    # The 'flipType' parameter flips the image, making it easier for some detections
+    hands, img = detector.findHands(img, draw=True, flipType=True)
+
+    if hands:
+        # Information for the first hand detected
+        hand = hands[0]  # Get the first hand detected
+        lmList = hand["lmList"]  # List of 21 landmarks for the first hand
+        # Count the number of fingers up for the first hand
+        fingers = detector.fingersUp(hand)
+        return fingers, lmList
+    else:
+        return None
+
+def draw(info, prev_pos):
+    fingers, lmList = info
+    current_pos = None
+
+    if fingers == [0, 1, 0, 0, 0]:
+        current_pos = lmList[8][0:2]
+        if prev_pos is None: 
+            prev_pos = current_pos
+        cv2.line(img, current_pos, prev_pos, (255, 0, 255), 10)
+    
+    return current_pos
+
+prev_pos = None
 
 # Continuously get frames from the webcam
 while True:
@@ -15,47 +47,13 @@ while True:
     # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
     success, img = cap.read()
 
-    # Find hands in the current frame
-    # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
-    # The 'flipType' parameter flips the image, making it easier for some detections
-    hands, img = detector.findHands(img, draw=True, flipType=True)
-
+    info = getHandInfo(img)
     # Check if any hands are detected
-    if hands:
-        # Information for the first hand detected
-        hand1 = hands[0]  # Get the first hand detected
-        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
-        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
-        center1 = hand1['center']  # Center coordinates of the first hand
-        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
-
-        # Count the number of fingers up for the first hand
-        fingers1 = detector.fingersUp(hand1)
-        print(f'H1 = {fingers1.count(1)}', end=" ")  # Print the count of fingers that are up
-
-        # Calculate distance between specific landmarks on the first hand and draw it on the image
-        length, info, img = detector.findDistance(lmList1[8][0:2], lmList1[12][0:2], img, color=(255, 0, 255),
-                                                    scale=10)
-
-        # Check if a second hand is detected
-        if len(hands) == 2:
-            # Information for the second hand
-            hand2 = hands[1]
-            lmList2 = hand2["lmList"]
-            bbox2 = hand2["bbox"]
-            center2 = hand2['center']
-            handType2 = hand2["type"]
-
-            # Count the number of fingers up for the second hand
-            fingers2 = detector.fingersUp(hand2)
-            print(f'H2 = {fingers2.count(1)}', end=" ")
-
-            # Calculate distance between the index fingers of both hands and draw it on the image
-            length, info, img = detector.findDistance(lmList1[8][0:2], lmList2[8][0:2], img, color=(255, 0, 0),
-                                                        scale=10)
-
-        print(" ")  # New line for better readability of the printed output
-
+    if info:
+        fingers, lmList = info
+        print(fingers)
+        prev_pos = draw(info, prev_pos)
+       
     # Display the image in a window
     cv2.imshow("Image", img)
 
